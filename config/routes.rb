d3367b25366060423c5dc54c923ce6b1db1b6717
ingridwong0715 <ingrid830715@gmail.com>
2018-08-08 22:17:50 -0400

    Add stats routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,26 +4,26 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'accounts', to: 'accounts#index' # ACCOUNT INDEX
-      post 'accounts', to: 'accounts#create' # ACCOUNT CREATE
-      get 'accounts/:account_number', to: 'accounts#show' # (HOME PAGE FOR DASHBOARD) ACCOUNTS SHOW
-      patch 'accounts/:account_number', to: 'accounts#update'# ACCOUNT UPDATE
-      delete 'accounts/:account_number', to: 'accounts#destroy' # ACCOUNT DESTROY
+      get 'accounts', to: 'accounts#index'
+      post 'accounts', to: 'accounts#create'
+      get 'accounts/:account_number', to: 'accounts#show'
+      patch 'accounts/:account_number', to: 'accounts#update'
+      delete 'accounts/:account_number', to: 'accounts#destroy'
 
-      get 'accounts/:account_number/bills', to: 'bills#index' #billing history
-      get 'accounts/:account_number/bills/:id', to: 'bills#show' # BILLS SHOW
-      get 'accounts/:account_number/bills/:start_date', to: 'bills#monthly_bill' #BILLS SHOW BY MONTH
+      get 'accounts/:account_number/bills', to: 'bills#index'
+      get 'accounts/:account_number/bills/:id', to: 'bills#show'
+      get 'accounts/:account_number/bills/:start_date', to: 'bills#monthly_bill'
       get 'accounts/:account_number/bills/unpaid', to: 'bills#unpaid_bills'
-      patch 'accounts/:account_number/bills/:id/pay', to: 'bills#update' # kind of similar to UPDATE
-
+      patch 'accounts/:account_number/bills/:id/pay', to: 'bills#update'
 
       scope '/admin', module: 'admin' do
         resources :bills
+        resources :accounts
         resources :users
-        get '/users/:email', to: 'users#email', constraints: { email: /.+@.+/} # Look up by user email
-        #more info on constraints: https://guides.rubyonrails.org/routing.html#advanced-constraints
+        get '/users/:email', to: 'users#email', constraints: { email: /.+@.+/}
         get '/stats/monthly_average_usage/:month/:year', to: 'stats#monthly_average_usage'
         get '/stats/user_activity', to: 'stats#user_activity'
+        get 'stats/account_breakdown', to: 'stats#account_type_breakdown'
       end
     end
   end
